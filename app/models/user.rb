052477080d077367906af43e9d050cfbe5cb572e
Danil <ogr.rng2016@yandex.ru>
2025-10-10 16:00:25 +0300

    Finish lab_8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,41 @@
-# app/models/user.rb
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false } # Нечувствительность к регистру
+            uniqueness: { case_sensitive: false }
   has_secure_password
-    validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  # Возвращает дайджест для строки
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Возвращает случайный токен
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Запоминает пользователя в БД для постоянной сессии
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Проверяет, соответствует ли токен дайджесту
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Забывает пользователя
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,26 +1,28 @@
 require 'test_helper'
 
-class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
-    get signup_path
-    assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",
-                                         email: "user@invalid",
-                                         password: "foo",
-                                         password_confirmation: "bar" } }
-    end
-    assert_template 'users/new'
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)  # Эта строка ищет фикстуру 'michael'
   end
 
-  test "valid signup information" do
-    get signup_path
-    assert_difference 'User.count', 1 do
-      post users_path, params: { user: { name: "Example User",
-                                         email: "user@example.com",
-                                         password: "password",
-                                         password_confirmation: "password" } }
-      follow_redirect!
-    end
+  test "login with valid information followed by logout" do
+    get login_path
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
     assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
